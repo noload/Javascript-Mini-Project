5003ec84ad3ffc59b8388ef9e5f6deceec588e98
noload <vaibhavkamble023@gmail.com>
2023-12-28 00:16:39 +0530

    added mini notification project

--- Toast Notification/script.js
+++ Toast Notification/script.js
@@ -1,10 +1,23 @@
 let toastBox = document.getElementById('toast-box');
-let successMsg = `Successfully submited`
-let errorMsg = `Please fix the error`
-let invalidMsg = ``
-function showToast() {
+let successMsg = `<i class='bx bxs-check-circle'></i>Successfully submited`
+let errorMsg = `<i class='bx bx-error'></i> Please fix the error`
+let invalidMsg = `<i class='bx bxs-error'></i>Invalid input,check again`
+function showToast(msg) {
     let toast = document.createElement('div');
     toast.classList.add('toast')
-    toast.innerHTML=`Success`
+    toast.innerHTML=msg
     toastBox.appendChild(toast);
+
+    if (msg.includes('error')) {
+        toast.classList.add('error')
+    }
+
+    if (msg.includes('invalid')) {
+        toast.classList.add('invalid')
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },6000)
 }
+
